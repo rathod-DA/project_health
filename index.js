de0bf016f24c8492c4c324c6430e3a8e4337ac6d
rathod-DA <202301164@dau.ac.in>
2025-11-25 16:47:05 +0530

    Chenged

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 const URL = process.env.URL || "";
 
 const check = async () => {
   try {
-    await axios.get(URL);
+    console.log("Url is, ",process.env.URL);
+    await axios.post(URL);
     console.log("Pinged");
   } catch (err) {
     console.log("Error pinging:", err.message);
   }
 };
 
-setInterval(check, 1000); 
+setInterval(check, 1000*60*2); 
 
 
 const awake = async () => {
